1ac6a82852a0da54ad6f016fe819e28d4469b244
Andrew Yue Xie <andrew@Andrews-MacBook-Air.local>
2016-09-10 22:54:45 -0700

    Module 1 Solution

---
+++ module1-solution/app.js
@@ -0,0 +1,34 @@
+(function() {
+  angular.module('myApp', [])
+  .controller('LunchCheckController', LunchCheckController)
+  .$inject('$scope');
+
+
+  function LunchCheckController ($scope) {
+    $scope.name = "item 1, item2,,,,i,item3";
+    $scope.message = "";
+    $scope.count = function () {
+      var s = $scope.name.trim();
+      if (s == "") {
+        $scope.status = "fail";
+        return $scope.message = "Please enter data first."
+      }
+      $scope.status = "success";
+
+      var arr = s.split(",");
+      var result = 0;
+
+      for (i of arr) {
+        if (i != "") {
+          result += 1;
+        }
+      }
+
+      if (result > 3) {
+        return $scope.message =  "Too much!";
+      } else {
+        return $scope.message =  "Enjoy!";
+      }
+    };
+  }
+})();
